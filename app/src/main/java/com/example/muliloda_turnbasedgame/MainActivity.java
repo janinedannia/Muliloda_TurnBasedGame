b9618e162512e6d966e7c0a60778e3dd1f2a83c8
Bambi <janinedannia@gmail.com>
2022-02-11 21:27:30 +0800

    Initial commit

---
+++ app/src/main/java/com/example/muliloda_turnbasedgame/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.muliloda_turnbasedgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextView txtHeroHP, txtEnemyHP, txtHeroDMG, txtEnemyDMG, txtCombatLog;
+    Button btnSlash;
+    ImageView mrDarcy = findViewById(R.id.mrDarcy);
+    ImageView mrWickham = findViewById(R.id.mrWickham);
+
+    String heroName = "Mr. Darcy";
+    int heroHP = 2000;
+    int heroMinDamage = 200;
+    int heroMaxDamage = 300;
+
+    String enemyName = "Mr. Wickham";
+    int enemyHP = 2500;
+    int enemyMinDamage = 100;
+    int enemyMaxDamage = 150;
+
+    int turnNumber = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtCombatLog = findViewById(R.id.txtCombatLog);
+        txtHeroHP = findViewById(R.id.txtHeroHP);
+        txtEnemyHP= findViewById(R.id.txtEnemyHP);
+        txtHeroDMG = findViewById(R.id.txtEnemyDMG);
+        btnSlash = findViewById(R.id.btnSlash);
+
+
+
+        txtHeroHP.setText(String.valueOf(heroHP));
+        txtHeroDMG.setText(String.valueOf(txtHeroDMG));
+        txtHeroDMG.setText("Damage per turn:" + heroMinDamage+ "-"+ heroMaxDamage);
+
+        txtEnemyHP.setText(String.valueOf(enemyHP));
+        txtEnemyDMG.setText(String.valueOf(txtEnemyDMG));
+        txtEnemyDMG.setText("Damage per turn "+ enemyMinDamage+ " - "+ enemyMaxDamage);
+
+
+        btnSlash.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+
+        Random randomizer = new Random();
+        int herodps = randomizer.nextInt((heroMaxDamage - heroMinDamage) + heroMaxDamage );
+        int enemydps = randomizer.nextInt((enemyMaxDamage - enemyMinDamage) + enemyMaxDamage );
+
+
+
+        switch(v.getId()) {
+            case R.id.btnSlash:
+                if (turnNumber%2 == 1) {
+                    mrDarcy.setVisibility(View.VISIBLE);
+                    mrWickham.setVisibility(View.VISIBLE);
+                    enemyHP = Math.max(0,enemyHP - txtHeroDMG);
+                    txtCombatLog.setText("Mr Darcy gave" + txtEnemyDMG + " damage to " + enemyName);
+                    txtEnemyHP.setText(String.valueOf(enemyHP));
+                    btnSlash.setText("Wickham's turn\nPress to proceed");
+                    turnNumber++;
+                    if (enemyHP == 0) {
+                        mrDarcy.setVisibility(View.INVISIBLE);
+                        turnNumber = 1;
+                        heroHP = 1000;
+                        enemyHP = 950;
+                        txtCombatLog.setText("Mr Darcy gave"+ txtHeroDMG+ " damage to "+ enemyName+ ". Elizabeth is mine, you rake!");
+                        btnSlash.setText("Rematch?");
+                    }
+                }
+                else if (turnNumber%2 != 1) {
+                    mrDarcy.setVisibility(View.VISIBLE);
+                    mrWickham.setVisibility(View.VISIBLE);
+                    heroHP = Math.max(0, heroHP - txtEnemyDMG);
+                    txtCombatLog.setText(enemyName + "gave" + txtEnemyDMG + "damage to" + heroName);
+                    txtHeroHP.setText(String.valueOf(heroHP));
+                    btnSlash.setText("Slash");
+                    turnNumber++;
+                    if (heroHP == 0) {
+                        mrWickham.setVisibility(View.INVISIBLE);
+                        turnNumber = 1;
+                        heroHP = 1000;
+                        enemyHP = 950;
+                        txtCombatLog.setText(enemyName+ " gave"+ txtEnemyDMG+ " damage to "+ heroName+ ". I won Elizabeth like I won your sister!");
+                        btnSlash.setText("Rematch?");
+                    }
+                }
+                break;
+        }
+
+
+    }
+}
+
+
